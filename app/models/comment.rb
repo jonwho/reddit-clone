29116a17efb6cd7aaadb47deb26d4403473a11c8
Jon Ho <lj080805@gmail.com>
2016-04-22 05:00:34 -0700

    complete parent/children association for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
-  belongs_to :parent, class_name: 'Comment'
+  belongs_to :parent, class_name: 'Comment', foreign_key: 'parent_id'
 
+  has_many :comments
 end
