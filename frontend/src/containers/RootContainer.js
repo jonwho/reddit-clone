fa36afd359a35e852082bafb157f4f49fee3ce62
Jon Ho <jonwho.contact@gmail.com>
2018-03-12 03:06:44 -0700

    added normalize.css
    removed unnecessary css files
    
    added RootContainer.js

---
+++ frontend/src/containers/RootContainer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class RootContainer extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Root Container</h1>
+      </div>
+    );
+  }
+}
+
+export default RootContainer;
