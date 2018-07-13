2e6c13ac9d4aba3581f4c1bc50169c03b068439e
Jon Ho <jonwho.contact@gmail.com>
2018-07-12 17:02:15 -0700

    Revert "Revert "move redux store outside of app""
    This reverts commit f9770066976d67bc7f37e9e884b8f9db693ff333.

--- frontend/src/containers/RootContainer.js
+++ frontend/src/containers/RootContainer.js
@@ -3,10 +3,6 @@ import HeaderContainer from './HeaderContainer';
 import SignupContainer from './SignupContainer';
 
 class RootContainer extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div>
