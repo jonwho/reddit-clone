e16f078464a52babb48dee435682843d1fbbe36f
Jon Ho <jonwho.contact@gmail.com>
2019-03-16 12:50:59 -0700

    wip - save for now and fix later

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -2,12 +2,19 @@ class ApplicationController < ActionController::API
   before_action :ensure_json_request
 
   def ensure_json_request
-    if params[:format] != 'json' && request.headers['Accept'] =~ /json/
-      render nothing: true, status: :not_found
-    end
+    return if json_request?
+
+    render nothing: true, status: :not_acceptable && return
   end
 
   def authorize_request
     render status: :unauthorized unless AuthorizationService.authorize(headers)
+  end
+
+  private
+
+  def json_request?
+    byebug
+    params[:format] == :json
   end
 end
---
+++ api/config/initializers/byebug.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# if Rails.env.development?
+#   Byebug.start_server 'localhost', ENV.fetch('BYEBUG_SERVER_PORT', 1048).to_i
+# end
